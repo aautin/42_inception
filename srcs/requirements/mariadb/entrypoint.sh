eda1760829d1993d5ddd00eadb3d672b976d445c
aautin <aautin@host.myguest.virtualbox.org>
2025-01-14 16:02:18 +0100

    create worpress database and an admin for it

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -10,10 +10,19 @@ y
 y
 END
 
-#mariadb users configuration
+#mariadb wordpress database configuration
 mariadb << END
-CREATE USER "$ADMIN_USERNAME"@"$ADMIN_HOST" IDENTIFIED BY "$ADMIN_PASSWORD";
+CREATE DATABASE wordpress;
+END
+
+#mariadb wordpress users configuration
+mariadb << END
+CREATE USER '$ADMIN_USERNAME'@'$ADMIN_HOST' IDENTIFIED BY '$ADMIN_PASSWORD';
 GRANT ALL PRIVILEGES ON * . * TO "$ADMIN_USERNAME"@"$ADMIN_HOST";
+
+CREATE USER '$WP_USERNAME'@'$WP_HOST' IDENTIFIED BY '$WP_PASSWORD';
+GRANT ALL PRIVILEGES ON wordpress.* TO '$WP_USERNAME'@'$WP_HOSTNAME';
+
 FLUSH PRIVILEGES;
 END
 
