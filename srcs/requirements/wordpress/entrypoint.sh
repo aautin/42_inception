3137ae1bc059ac0800395e3e04cd411daf748396
root <root@host.myguest.virtualbox.org>
2025-01-15 17:39:05 +0100

    update wordpress Dockerfile/entrypoint, not connected to mariadb

--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
-tail -f /dev/null
+#install wp-cli
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
+
+##install wordpress
+mkdir -p /var/www/html
+cd /var/www/html
+wp core download --allow-root
+wp config create --dbname=$DB_NAME --dbuser=$WP_USERNAME --dbpass=$WP_PASSWORD --allow-root --dbhost=mariadb:3306
+
+#start php in foreground
+exec php-fpm7.4 --nodaemonize
